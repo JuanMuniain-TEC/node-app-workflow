87b6c01beee43b4d23ae8207bb09aa75f1d7f6cf
munij3 <a01781341@tec.mx>
2023-06-12 14:46:46 -0600

    Update index.js

--- A01781341/index.js
+++ A01781341/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('This is my express app')
+  res.send('This is my express app.')
 })
 
 app.get('/hello', (req, res) => {
